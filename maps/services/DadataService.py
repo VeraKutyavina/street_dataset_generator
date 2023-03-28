9712a7ffd450a7f8de070545d5220820e20e3f4f
VeraKutyavina <vera.kutiavina@flatstack.com>
2023-03-28 13:13:08 +0300

    add check array of addresses

--- maps/services/DadataService.py
+++ maps/services/DadataService.py
@@ -4,7 +4,19 @@ token = "8809edcac820bf5caaaa1ef4d8071259815212de"
 secret = "fc0528818eb61d5947a3406e42982182e7e5027f"
 
 
+def get_address(item):
+    return item['value']
+
+
 def get_address_by_coord(x, y):
     dadata = Dadata(token)
     result = dadata.geolocate(name="address", lat=float(x), lon=float(y))
-    return result
+    addresses_array = list(map(get_address, result))
+    print(addresses_array)
+    return addresses_array
+
+
+def get_street_by_coord(x, y):
+    dadata = Dadata(token)
+    result = dadata.geolocate(name="address", lat=float(x), lon=float(y))
+    return result[0]['data']['street']
--- maps/services/VideoServices.py
+++ maps/services/VideoServices.py
@@ -4,7 +4,7 @@ import glob
 
 from playwright.sync_api import sync_playwright
 
-from maps.services.DadataService import get_address_by_coord
+from maps.services.DadataService import get_address_by_coord, get_street_by_coord
 from maps.services.YandexService import get_coord_by_address
 
 MAPS_URL = 'http://127.0.0.1:8000/maps/'
@@ -30,8 +30,8 @@ def record_video_with_playwright(coordinates, address):
         page = context.new_page()
         page.goto(url)
         page.wait_for_timeout(1000)
-        current_address = address
-        while address in current_address:
+        current_address = [address]
+        while any(address in x for x in current_address):
             page.keyboard.press('ArrowUp')
             page.keyboard.press('ArrowUp')
 
@@ -39,7 +39,7 @@ def record_video_with_playwright(coordinates, address):
             y = page.get_by_test_id('coordinate-y').all_inner_texts()[0]
 
             if not x == '' and not y == '':
-                current_address = get_address_by_coord(x, y)[0]['value']
+                current_address = get_address_by_coord(x, y)
                 print(current_address)
             page.screenshot(path='video-images-opencv/image' + str(i) + '.png')
             i += 1
@@ -53,8 +53,8 @@ def record_video_with_playwright(coordinates, address):
         page = context.new_page()
         page.goto(url)
         page.wait_for_timeout(1000)
-        current_address = address
-        while address in current_address:
+        current_address = [address]
+        while any(address in x for x in current_address):
             page.keyboard.press('ArrowDown')
             page.keyboard.press('ArrowDown')
 
@@ -62,7 +62,7 @@ def record_video_with_playwright(coordinates, address):
             y = page.get_by_test_id('coordinate-y').all_inner_texts()[0]
 
             if not x == '' and not y == '':
-                current_address = get_address_by_coord(x, y)[0]['value']
+                current_address = get_address_by_coord(x, y)
                 print("second")
                 print(current_address)
             page.screenshot(path='video-images-opencv/image' + str(i) + '.png')
@@ -74,7 +74,7 @@ def record_video_with_playwright(coordinates, address):
 
 def create_map_video(address):
     coordinates = get_coord_by_address(address)
-    street = get_address_by_coord(coordinates[1], coordinates[0])[0]['data']['street']
+    street = get_street_by_coord(coordinates[1], coordinates[0])
     record_video_with_playwright(coordinates, street)
     # convert_webm_to_mp4()
 
