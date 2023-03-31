4115c6297ac206d243611d1e3e8fb72f5674bc5b
VeraKutyavina <vera.kutiavina@flatstack.com>
2023-03-31 14:17:35 +0400

    fix

--- maps/services/OSMService.py
+++ maps/services/OSMService.py
@@ -25,11 +25,7 @@ def get_random_points(address):
     return [point1, point2]
 
 
-def get_street_in_city(place_name):
-    # получаем граф дорог города
-    graph = ox.graph_from_place(place_name, network_type='all')
-
-    # получаем список всех улиц города
+def get_streets_name(graph):
     streets = []
     for u, v, k, data in graph.edges(keys=True, data=True):
         if 'highway' and 'name' in data.keys():
@@ -37,29 +33,25 @@ def get_street_in_city(place_name):
             if street_name not in streets and isinstance(street_name, str):
                 streets.append(street_name)
 
+    return streets
+
+
+def get_street_in_city(place_name):
+    # graph of available city roads
+    graph = ox.graph_from_place(place_name, network_type='all')
+    streets = get_streets_name(graph)
+
     print(streets)
     print(len(streets))
 
 
 def get_street_in_place():
-    # координаты прямоугольника
+    # rectangle coordinates
     north, south, east, west = 55.79, 55.82, 49.16, 49.11
+    graph = ox.graph_from_bbox(north, south, east, west, network_type="all")
 
-    tags = {"highway": True}
+    streets = get_streets_name(graph)
 
-    # Получение графа дорожной сети области
-    G = ox.graph_from_bbox(north, south, east, west, network_type="all")
-
-    print('HI')
-
-    # Получение списка всех улиц области
-    streets = []
-    for u, v, k, data in G.edges(keys=True, data=True):
-        if 'highway' and 'name' in data.keys():
-            street_name = data['name']
-            if street_name not in streets and isinstance(street_name, str):
-                streets.append(street_name)
-
-    # Вывод списка улиц
     print(streets)
+    print(len(streets))
 
