cea67226310c369766fe45ed9fc5c476f168b312
VeraKutyavina <vera.kutiavina@flatstack.com>
2023-03-31 15:42:13 +0400

    add fields in html

--- maps/services/MLService.py
+++ maps/services/MLService.py
@@ -9,7 +9,6 @@ def counting_cars():
     vehicles_folder_count = 0
 
     for img_path in images_folder:
-        print("Img path", img_path)
         img = cv2.imread(img_path)
         vehicle_boxes = vd.detect_vehicles(img)
         vehicle_count = len(vehicle_boxes)
